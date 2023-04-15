d4fc984ad1d4923fc80cd1cbd82dd5b3025cdf06
Andres Pulupa <andrespulupa18@gmail.com>
2023-04-15 15:06:51 -0500

    Update CV link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
             <ul className='flex gap-4 mt-3 flex-wrap justify-center'>
               <li><a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href='https://github.com/andresdeveloper01' target='_blank' rel='noreferrer'>GitHub <BsGithub /></a></li>
               <li><a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href='https://www.linkedin.com/in/andres-pulupa/' target='_blank' rel='noreferrer'>Linkedin <AiFillLinkedin /></a></li>
-              <li><a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href='https://drive.google.com/file/d/1B6gjH5ws_Z3SQ6IIjlTjMtZY2YoOVLZ_/view' target='_blank' rel='noreferrer'>CV <CiFolderOn /></a></li>
+              <li><a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href='https://drive.google.com/file/d/1cuAvvbQPAs58FqiaJk6qogZDeyNpRh6B/view' target='_blank' rel='noreferrer'>CV <CiFolderOn /></a></li>
               <li><a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href='mailto:andrespulupa18@gmail.com' target='_blank' rel='noreferrer'>Contactame <FiMail /></a></li>
             </ul>
           </div>
