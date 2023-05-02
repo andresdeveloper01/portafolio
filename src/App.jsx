02076897ddac5ca2e20cf4a5158c169994363b60
Andres <andres@pulupa.com>
2023-05-02 15:26:24 -0500

    added particles background

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import AboutMe from './components/AboutMe'
-import Header from './components/Header'
-import Projects from './components/Projects'
-import { IntersectionObserverProvider } from './context/IntersectionObserver'
+import ParticlesBackground from "./ParticlesBackground";
+import AboutMe from "./components/AboutMe";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import { IntersectionObserverProvider } from "./context/IntersectionObserver";
 
-function App () {
-  return (
-    <>
-      <IntersectionObserverProvider>
-        <Header />
-        <AboutMe />
-        <Projects />
-      </IntersectionObserverProvider>
-    </>
-  )
+function App() {
+	return (
+		<>
+			<ParticlesBackground />
+			<IntersectionObserverProvider>
+				<Header />
+				<AboutMe />
+				<Projects />
+			</IntersectionObserverProvider>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-
 import { FiMail } from 'react-icons/fi'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { BsGithub } from 'react-icons/bs'
@@ -10,10 +9,26 @@ import { useContext } from 'react'
 import { IntersectionObserver } from '../context/IntersectionObserver'
 
 const links = [
-  { url: 'https://github.com/andresdeveloper01', title: 'GitHub', icon: <BsGithub /> },
-  { url: 'https://www.linkedin.com/in/andres-pulupa/', title: 'Linkedin', icon: <AiFillLinkedin /> },
-  { url: 'https://docs.google.com/document/d/1Qsyjep9YkoGkFQ3uIk8Q4TVFFLoSgSIgRzOXRrTQXio/edit', title: 'CV', icon: <CiFolderOn /> },
-  { url: 'mailto:andrespulupa18@gmail.com', title: 'Contactame', icon: <FiMail /> }
+  {
+    url: 'https://github.com/andresdeveloper01',
+    title: 'GitHub',
+    icon: <BsGithub />
+  },
+  {
+    url: 'https://www.linkedin.com/in/andres-pulupa/',
+    title: 'Linkedin',
+    icon: <AiFillLinkedin />
+  },
+  {
+    url: 'https://docs.google.com/document/d/1Qsyjep9YkoGkFQ3uIk8Q4TVFFLoSgSIgRzOXRrTQXio/edit',
+    title: 'CV',
+    icon: <CiFolderOn />
+  },
+  {
+    url: 'mailto:andrespulupa18@gmail.com',
+    title: 'Contactame',
+    icon: <FiMail />
+  }
 ]
 
 const Header = () => {
@@ -21,16 +36,31 @@ const Header = () => {
   return (
     <header>
       <Nav />
-      <section id='home' ref={ref1} className='min-h-[77vh] flex justify-center items-center mt-14'>
+      <section
+        id='home'
+        ref={ref1}
+        className='min-h-[77vh] flex justify-center items-center mt-14'
+      >
         <div className='flex flex-col items-center min-[1400px]:gap-32 gap-5 mx-auto h-full w-fit sm:max-w-[610px] md:max-w-[720px] md:flex-row min-[990px]:max-w-[960px] min-[1200px]:max-w-[1160px] min-[1400px]:max-w-[1340px]'>
           <div className='flex flex-col items-center min-[990px]:gap-7 min-[400px]:mb-16 md:mb-0'>
             <p className='text-2xl'>¡Hola!</p>
-            <h1 className='text-5xl font-bold text-center mb-3'> Soy Andres Pulupa</h1>
-            <p className='text-3xl font-medium text-center'>Desarrollador Frontend</p>
+            <h1 className='text-5xl font-bold text-center mb-3'>
+              {' '}
+              Soy Andres Pulupa
+            </h1>
+            <p className='text-3xl font-medium text-center'>
+              Desarrollador Frontend
+            </p>
             <ul className='flex gap-4 mt-3 flex-wrap justify-center'>
               {links.map(({ url, title, icon }) => (
                 <li key={url}>
-                  <a className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300' href={url} target='_blank' rel='noreferrer' aria-label='Link to my social networks'>
+                  <a
+                    className='flex gap-1 items-center transition ease-in-out delay-150 hover:scale-110 duration-300'
+                    href={url}
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label='Link to my social networks'
+                  >
                     {title}
                     {icon}
                   </a>
@@ -39,7 +69,11 @@ const Header = () => {
             </ul>
           </div>
           <div className='sm:ml-9'>
-            <img className='w-36 min-[400px]:w-44 min-[400px]:h-44  md:w-52 md:h-52 sm:w-48 sm:h-48 rounded-[500px] h-36 object-cover min-[990px]:w-80 min-[990px]:h-80 min-[1200px]:w-[500px] min-[1200px]:h-[500px]' src={photOfMe} alt='foto de perfil' />
+            <img
+              className='w-36 min-[400px]:w-44 min-[400px]:h-44  md:w-52 md:h-52 sm:w-48 sm:h-48 rounded-[500px] h-36 object-cover min-[990px]:w-80 min-[990px]:h-80 min-[1200px]:w-[500px] min-[1200px]:h-[500px]'
+              src={photOfMe}
+              alt='foto de perfil'
+            />
           </div>
         </div>
       </section>
